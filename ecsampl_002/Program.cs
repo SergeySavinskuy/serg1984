bc51cfed3d43b9d390ba7860634599b7048f0f64
sergeSavinsky <tsavinsky@yandex.ru>
2023-02-19 17:20:08 +0300

    написали программу по задаче 4

---
+++ ecsampl_002/Program.cs
@@ -0,0 +1,19 @@
+﻿// программа принимает на вход три числа и выдает максимальное
+Console.WriteLine("Введите первое число");
+int namberA = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int namberB = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int namberC = int.Parse(Console.ReadLine());
+int macs = namberA;
+if (namberA < namberB)
+{
+    macs = namberB;
+}
+if (namberB < namberC)
+{
+    macs = namberC;
+}
+
+Console.Write("Максимальное из трех чисел");
+Console.WriteLine(macs);
