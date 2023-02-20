cb99704f14af38c40b2538a6bbdedaa11b796c08
sergeSavinsky <tsavinsky@yandex.ru>
2023-02-21 00:35:25 +0300

    задача на четность числа

---
+++ ecsampl_003/Program.cs
@@ -0,0 +1,12 @@
+﻿// программа принимает число и выдает является ли число четным
+Console.WriteLine("Введите число");
+int namber= int.Parse(Console. ReadLine());
+if  (namber%2==0)
+{
+    Console.WriteLine("Данное число является четным");
+}
+else
+{
+    Console.WriteLine("Число не является четным");
+}
+
