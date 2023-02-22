9ccbb7bf6d4419eb039e78e6a97c07e7017fcb17
sergeSavinsky <tsavinsky@yandex.ru>
2023-02-22 16:16:25 +0300

    написали  программу которая принимает число и выдает четные числа от 1 до N

---
+++ ecsampl_004/Program.cs
@@ -0,0 +1,12 @@
+﻿// Программа принимает число N и показывает все четные числа от 1 до N
+Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+int a=1;
+while (a<=N)
+{
+if (a%2==0)
+{
+    Console.WriteLine(a);
+}
+a++;
+}
