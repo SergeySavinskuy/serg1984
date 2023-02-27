8ed0bbc28034c613bff546a717b0911e6312953f
sergeSavinsky <tsavinsky@yandex.ru>
2023-02-27 14:29:20 +0300

    задача на показ второй цифры трехзначного числа

---
+++ ecsampl_010/Program.cs
@@ -0,0 +1,19 @@
+﻿// Программа принимает на вход трехзнаное число и на выходе показывает вторую цифру этого числа
+
+int EnterNamber(string message)
+{
+    Console.Write(message);
+   string value = Console.ReadLine();
+  int resalt = Convert.ToInt32(value);
+    return resalt;
+}
+
+int namber = EnterNamber("Введите трехзначное число >");
+if (namber < 100 || namber >= 1000)
+{
+    System.Console.WriteLine("Введено некорректное число");
+    return;
+}
+System.Console.WriteLine($"Введенное число'{namber}'");
+int secondNamber = namber / 10 % 10;
+System.Console.WriteLine($"Вторая цифра введеного числа'{secondNamber}'");
