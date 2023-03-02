da27a3580a34b706bc81b6ebe001d8ea449d73de
sergeSavinsky <tsavinsky@yandex.ru>
2023-03-03 00:14:31 +0300

    задача 13

--- ecsampl_013/Program.cs
+++ ecsampl_013/Program.cs
@@ -1,49 +1,45 @@
-﻿// Программа принимает число и показывает третье число или показывает что третьего числа нет
-
+﻿  
+// Программа принимает число и показывает третье число или показывает что третьего числа неint EnterNamber(string message)
 int EnterNamber(string message)
 {
     Console.Write(message);
-   string value = Console.ReadLine();
-  int resalt = Convert.ToInt32(value);
+    string value = Console.ReadLine()!;
+    int resalt = Convert.ToInt32(value);
     return resalt;
 }
 // создание массива
-int[]CreateArrai(int size)
+int[] CreateArrai(int size)
 {
     return new int[size];
 }
 
 string Print(int[] array)
 {
-  string result = String.Empty;
-  int len = array.Length;
-  int index = 0;
-  while (index < len)
-  {
-    result = result + " " + array[index];
-    index++;
-  }
-  return result;
+    string result = String.Empty;
+    int len = array.Length;
+    int index = 0;
+    while (index < len)
+    {
+        result = result + " " + array[index];
+        index++;
+    }
+    return result;
 }
 
-void FillByNamber(int[] array)
-{
-int namber=EnterNamber("Ведите число");
-int size = array.Length;
-int index=0;
-while (index<size)
-{
-    
-    array[index]=namber%10;
-   index= index+1;
-   namber= namber/10;
-}
-
-}
-int namber=EnterNamber("еше раз повторите введенное число");
+int namber = EnterNamber("Ведите число  ");
 double i = Math.Log10(namber);
-int size = (int)i+1;
-int[] number = CreateArrai(size);
-System.Console.WriteLine(Print(number));
- FillByNamber(number);
-System.Console.WriteLine(Print(number));
+int size = (int)i + 1;
+int[]arrai=CreateArrai(size);
+ for (int index = size-1; index>-1; index--)
+ {
+    arrai[index]=namber%10;
+    namber=namber/10;
+ }
+System.Console.WriteLine(Print(arrai));
+int nom=EnterNamber("Введите какую цифру числа показать ");
+  if (arrai.Length>nom)
+  {
+   // Console.WriteLine(arrai[nom-1]); // вариант если считать слева на право
+    Console.WriteLine(arrai[size-nom]);
+  }
+  else Console.Write(" цифры в числе  нет");
